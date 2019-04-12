d0e07fdcfb2c9ad571fa6ddf4882aec2e90c6a35
kiddick <kiddick@users.noreply.github.com>
2019-04-12 14:18:10 +0300

    Add todos component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,36 @@
 <template>
-  <div id="app">{{msg}}</div>
+  <div id="app">
+    <Todos/>
+  </div>
 </template>
 
 <script>
+import Todos from "./components/Todos";
+
 export default {
   name: "app",
-  components: {},
+  components: {
+    Todos
+  },
   data() {
     return {
-      msg: "Hello"
+      todos: [
+        {
+          id: 1,
+          title: "Todo 1",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo 2",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Todo 3",
+          completed: false
+        }
+      ]
     };
   }
 };
